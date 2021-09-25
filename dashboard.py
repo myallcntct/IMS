86b7fae6dc65c535659ba8095e32607ba50544d9
Abdullah Al Mamun <myallcntct@gmail.com>
2021-09-25 22:45:08 +0600

    first commit

---
+++ dashboard.py
@@ -0,0 +1,36 @@
+from tkinter import*
+from PIL import Image,ImageTk #pip install pillow
+class IMS:
+    def __init__(self, root):
+        self.root=root
+        self.root.geometry("1300x700+0+0")
+        self.root.title("Inventory Management System | Development by Mamun")
+        self.root.config(bg="white")
+
+        #========title======
+        self.icon_title=PhotoImage(file="images/logo1.png")
+        title=Label(self.root,text="Inventory Management System",image=self.icon_title,compound=LEFT,font=("times new roman", 40,"bold"),bg="#010c48",fg="white",anchor="w",padx=20).place(x=0,y=0,relwidth=1,height=70)
+
+        #====Button logout=======
+        btn_logout=Button(self.root,text="Logout",font=("times new roman",15,"bold"),bg="yellow",cursor="hand2").place(x=1100,y=10,height=50,width=150)
+
+        #=========Clock========
+        self.lbl_clock=Label(self.root,text="Welcome to Inventory Management System\t\t Date: DD-MM-YYYY\t\t Time: HH:MM:SS",font=("times new roman", 15),bg="#4d636d",fg="white")
+        self.lbl_clock.place(x=0,y=70,relwidth=1,height=30)
+
+        #========Left Menu========
+        self.MenuLogo=Image.open("images/menu_im.jpg")
+        self.MenuLogo=self.MenuLogo.resize((200,200),Image.ANTIALIAS)
+        self.MenuLogo=ImageTk.PhotoImage(self.MenuLogo)
+
+        leftMenu=Frame(self.root,bd=2,relief=RIDGE,bg="white")
+        leftMenu.place(x=0,y=102,width=200,height=565)
+        lbl_menuLogo=Label(leftMenu,image=self.MenuLogo)
+        lbl_menuLogo.pack(side=TOP,fill=X)
+
+        lbl_menu=Label(leftMenu,text="Menu", font=("times new roman",20),bg="#009688").pack(side=TOP,fill=X)
+        btn_employee=Button(leftMenu,text="Employee", font=("times new roman",20,"bold"),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+
+root=Tk()
+obf=IMS(root)
+root.mainloop()
